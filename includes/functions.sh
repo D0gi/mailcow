9417b54e552c09a63ff4cb583339342c04afccd4
Dogi <s122106@hft-leipzig.de>
2015-09-02 19:40:23 +0200

    Update functions.sh
    force yes for instal dbus

--- includes/functions.sh
+++ includes/functions.sh
@@ -180,7 +180,7 @@ EOF
 			echo "$(textb [INFO]) - Setting your hostname..."
 			if [[ -f /lib/systemd/systemd ]]; then
 				if [[ -z $(dpkg --get-selections | grep -E "^dbus.*install$") ]]; then
-					apt-get update -y > /dev/null 2>&1 && apt-get install dbus > /dev/null 2>&1
+					apt-get update -y > /dev/null 2>&1 && apt-get install -y dbus > /dev/null 2>&1
 				fi
 				hostnamectl set-hostname ${sys_hostname}
 			else
